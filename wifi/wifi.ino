c606d15efca5163ab3e512474f2f3366e84689f6
Petr Posvic <petr@posvic.cz>
2016-06-12 18:43:50 +0200

    Remove ByteCounter, add some chars conditions

--- wifi/wifi.ino
+++ wifi/wifi.ino
@@ -1,45 +1,41 @@
 #include <ESP8266WiFi.h>
 #include <ArduinoJson.h>
 
-const char* ssid     = "ssid";
-const char* password = "pass";
-const char* host     = "192.168.1.203";
-const int   port     = 3000;
+const char* ssid = "ssid";
+const char* pass = "pass";
+const char* host = "192.168.1.203";
+const int   port = 3000;
 
-StaticJsonBuffer<200> jsonBuffer;
-JsonObject& root = jsonBuffer.createObject();
-String data = "";
-
-class ByteCounter : public Print {
-  public:
-    ByteCounter(): len(0) {}
-
-    virtual size_t write(uint8_t c) {
-      len++;
-    }        
-
-    int length() const {
-      return len;
-    }
-
-  private:
-    int len;
-};
+// Store icoming data from serial, then send via network
+String data = String();
+char ch;
 
 void setup() {
   Serial.begin(9600);
 
-  WiFi.begin(ssid, password);
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, pass);
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
   }
-
-  root["device"] = "petr";
 }
 
 void loop() {
   while (Serial.available() > 0) {
     char ch = Serial.read();
+
+    if (
+      // Number
+      (ch < 48 || ch > 57) &&
+
+      // .
+      ch != 46 &&
+
+      // +
+      ch != 43
+    ) {
+      continue;
+    }
 
     // Ignore + character, only for debugging
     if (ch == '+') {
@@ -48,34 +44,44 @@ void loop() {
     }
 
     if (ch == '.') {
-      root["value"] = data;
-      sendData();
-
+      send_data();
       data = "";
-      break;
+      continue;
     }
 
     data += ch;
   }
+
+  delay(1000);
 }
 
-void sendData() {
-  WiFiClient client;
-  if (client.connect(host, port)) {
-    ByteCounter counter;
-    root.printTo(counter);
-    int contentLength = counter.length();
+void send_data() {
 
-    client.println("POST /api/sleeps HTTP/1.1");
-    client.print("Host: "); client.println(host);
-    client.println("Cache-Control: no-cache");
-    client.println("Content-Type: application/json");
-    client.print("Content-Length: ");
-    client.println(contentLength);
-    client.println();
-    root.printTo(client);
-    client.println();
-    delay(500);
+  // Avoid sending empty data
+  if (data.length() < 1) {
+    return;
   }
+
+  WiFiClient client;
+  if (!client.connect(host, port)) {
+    delay(1000);
+    return;
+  }
+
+  DynamicJsonBuffer jsonBuffer;
+  JsonObject& root = jsonBuffer.createObject();
+  root["device"] = "petr";
+  root["value"] = data;
+
+  client.println("POST /api/sleeps HTTP/1.1");
+  client.print("Host: "); client.println(host);
+  client.println("Cache-Control: no-cache");
+  client.println("Content-Type: application/json");
+  client.print("Content-Length: "); client.println(root.measureLength());
+  client.println();
+  root.printTo(client);
+  client.println();
+
+  delay(5000);
 }
 
