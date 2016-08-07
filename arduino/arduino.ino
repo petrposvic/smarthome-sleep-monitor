6202008ae02cc5c5933e6e1c01bb1adac2563ff3
Petr Posvic <petr@posvic.cz>
2016-08-07 16:58:14 +0200

    Use DHT22 to send temperature and humidity

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -1,16 +1,18 @@
 #include <Wire.h>
 #include <SoftwareSerial.h>
+#include "DHT.h"         // https://github.com/adafruit/DHT-sensor-library
+
+#define DHTPIN 2
+#define DHTTYPE DHT22
 
 const int MPU_addr = 0x68;
 const int interval = 5 * 60 * 10;
 
 SoftwareSerial wifi(10, 11); // RX, TX
+DHT dht(DHTPIN, DHTTYPE);
 
-int16_t ax, ay, az;
 int16_t gx, gy, gz;
-int16_t tmp;
 int16_t lx = 0, ly = 0, lz = 0; // Last values
-int dx, dy, dz;                 // Differences
 
 long sum = 0;
 int count = interval - 5 * 10;
@@ -20,12 +22,12 @@ void setup() {
   // Debug serial
   Serial.begin(9600);
   Serial.println("debug serial");
-  delay(1000);
+  delay(100);
 
   // Wifi serial
   wifi.begin(9600);
   Serial.println("wifi serial");
-  delay(1000);
+  delay(100);
 
   // MPU6050
   Wire.begin();
@@ -34,7 +36,11 @@ void setup() {
   Wire.write(0);
   Wire.endTransmission(true);
   Serial.println("mpu6050");
-  delay(1000);
+  delay(100);
+
+  // DHT22
+  dht.begin();
+  Serial.println("dht22");
 }
 
 void loop() {
@@ -66,33 +72,9 @@ void loop() {
 
     Serial.println(sum);
 
-    String payload = "{";
-    payload = payload + "\"device\":\"petr\",";
-    payload = payload + "\"value\":\"" + sum + "\"";
-    payload = payload + "}";
-
-    String msg = "POST /api/sleeps HTTP/1.1\r\n";
-    msg = msg + "Host:192.168.1.203\r\n";
-    msg = msg + "Cache-Control:no-cache\r\n";
-    msg = msg + "Content-Type:application/json\r\n";
-    msg = msg + "Content-Length:" + payload.length() + "\r\n";
-    msg = msg + "\r\n";
-    msg = msg + payload + "\r\n";
-    msg = msg + "\r\n";
-
-    /*Serial.println(msg);
-    Serial.println(payload);
-    Serial.print("payload size = ");
-    Serial.println(payload.length());
-    Serial.print("msg size = ");
-    Serial.println(msg.length());*/
-
-    String cmd = String();
-    cmd = cmd + "AT+CIPSEND=" + msg.length() + "\r\n";
-
     send_at_cmd("AT+CIPSTART=\"TCP\",\"192.168.1.203\",3000\r\n");
-    send_at_cmd(cmd);
-    send_at_cmd(msg);
+    send_sleep();
+    send_dht();
     send_at_cmd("AT+CIPCLOSE\r\n");
 
     sum = 0;
@@ -124,7 +106,62 @@ void send_at_cmd(String cmd) {
   }
 }
 
+void send_post(String path, String payload) {
+  String msg = "";
+  msg = msg + "POST " + path + " HTTP/1.1\r\n";
+  msg = msg + "Host:192.168.1.203\r\n";
+  msg = msg + "Cache-Control:no-cache\r\n";
+  msg = msg + "Content-Type:application/json\r\n";
+  msg = msg + "Content-Length:" + payload.length() + "\r\n";
+  msg = msg + "\r\n";
+  msg = msg + payload + "\r\n";
+  msg = msg + "\r\n";
+
+  /*Serial.print("msg = ");
+  Serial.println(msg);
+  Serial.println(payload);
+  Serial.print("payload size = ");
+  Serial.println(payload.length());
+  Serial.print("msg size = ");
+  Serial.println(msg.length());*/
+
+  String cmd = "";
+  cmd = cmd + "AT+CIPSEND=" + msg.length() + "\r\n";
+  send_at_cmd(cmd);
+  send_at_cmd(msg);
+}
+
+void send_sleep() {
+  if (sum < 0) {
+    Serial.println("ignore negative values!");
+    return;
+  }
+
+  String payload = "{\"device\":\"petr\",\"value\":\"";
+  payload = payload + sum + "\"}";
+  send_post("/api/sleeps", payload);
+}
+
+void send_dht() {
+  float h = dht.readHumidity();
+  float t = dht.readTemperature();
+  if (isnan(h) || isnan(t)) {
+    Serial.println("failed to read from DHT sensor!");
+    return;
+  }
+
+  float hic = dht.computeHeatIndex(t, h, false);
+  Serial.println(t);
+  Serial.println(h);
+
+  String payload = "{\"dev\":\"bedroom\",\"t\":\"";
+  payload = payload + t + "\",\"h\":\"";
+  payload = payload + h + "\"}";
+  send_post("/api/measurements", payload);
+}
+
 void read_mpu() {
+  int16_t ax, ay, az, tmp;
   Wire.beginTransmission(MPU_addr);
   Wire.write(0x3B);
   Wire.endTransmission(false);
